93ab63a8b7c0dfd1d7e81023ed6c9828f1163f52
Afonso Bordado <afonsobordado@az8.co>
2023-04-06 10:39:17 +0100

    Triple Cleaning

--- src/compiler.rs
+++ src/compiler.rs
@@ -13,6 +13,28 @@ use target_lexicon::Triple;
 
 use crate::cli::AppArgs;
 
+fn parse_triple(triple: &str) -> Result<Triple, target_lexicon::ParseError> {
+    let cleantriple = if triple.contains("msvc") {
+        let parts = triple.split("-");
+        let mut new_triple = String::new();
+        for part in parts {
+            // Clang on windows sometimes returns a msvc triple with the
+            // version number, e.g. x86_64-pc-windows-msvc19.0.24215.1
+            // target-lexicon doesn't support this, so we strip it out
+            if part.starts_with("msvc") {
+                new_triple.push_str("msvc");
+            } else {
+                new_triple.push_str(part);
+            }
+        }
+        new_triple
+    } else {
+        triple.to_string()
+    };
+
+    Triple::from_str(&cleantriple)
+}
+
 pub struct Compiler {
     module: ObjectModule,
     triple: Triple,
@@ -23,7 +45,7 @@ impl Compiler {
     pub fn new(args: AppArgs, tu: TranslationUnit) -> Self {
         let target = tu.get_target();
         let name = args.output.file_name().unwrap().to_str().unwrap();
-        let triple = Triple::from_str(&target.triple).expect("Invalid triple");
+        let triple = parse_triple(&target.triple).expect("Invalid triple");
 
         let isa = {
             let mut flag_builder = settings::builder();
