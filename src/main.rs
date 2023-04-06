7bed94076a27af630459dd792964b9c9dd6180b1
Afonso Bordado <afonsobordado@az8.co>
2023-04-06 01:01:39 +0100

    Github CI

--- src/main.rs
+++ src/main.rs
@@ -46,32 +46,6 @@ fn main() -> Result<()> {
 
     let compiler = Compiler::new(args.clone(), tu);
 
-    // let lowerer = AstLowerer::new(ast)?;
-    // let ir = match lowerer.lower() {
-    //     Ok(ir) => ir,
-    //     Err(e) => {
-    //         eprintln!("Error lowering IR for file {}\n{}", args.input.display(), e);
-    //         std::process::exit(1);
-    //     }
-    // };
-
-    // if args.dump_ir {
-    //     for func in &ir {
-    //         println!("{:#?}", func);
-    //     }
-    // }
-
-    // let flag_builder = settings::builder();
-    // let isa_builder = cranelift::codegen::isa::lookup_by_name("x86_64-unknown-linux-gnu")?;
-    // let isa = isa_builder.finish(settings::Flags::new(flag_builder))?;
-    // let mut module = ObjectModule::new(ObjectBuilder::new(isa, "foo", default_libcall_names())?);
-
-    // for func in ir {
-    //     let func_id = module.declare_function("main", Linkage::Local, &func.signature)?;
-    //     let mut ctx = Context::for_function(func);
-    //     module.define_function(func_id, &mut ctx)?;
-    // }
-
     let module = compiler.finish();
     let obj = module.finish();
     let bytes = obj.emit()?;
